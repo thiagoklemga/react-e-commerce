f2436b237045ec625619c8f1c917f1ce802c899e
thiagoklemga <thiagoklemg@outlook.com>
2023-03-02 21:49:03 -0300

    fix: App.test

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
-  return !isAuthenticated ? (
-    <button onClick={() => loginWithRedirect()}>Login</button>
-  ) : (
+  return (
     <>
-      <button onClick={() => logout()}>Log out</button>
-      {JSON.stringify(user)}
+      <h1>Hey, its working!</h1>
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      ) : (
+        <>
+          <button onClick={() => logout()}>Log out</button>
+          {JSON.stringify(user)}
+        </>
+      )}
     </>
   );
 }

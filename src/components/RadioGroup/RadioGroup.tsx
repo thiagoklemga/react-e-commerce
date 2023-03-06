0fba630d370f5c460ea3f36b6370fe86bea9a4a6
thiagoklemga <thiagoklemg@outlook.com>
2023-03-06 16:40:15 -0300

    feat: filter products by category

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { useState } from 'react';
+import { RadioGroup as HeadlesUiRadioGroup } from '@headlessui/react';
+
+interface RadioGroupProps {
+  value: string;
+  onChange: (value: string) => void;
+  options: {
+    value: string;
+    label: string;
+  }[];
+  className?: string;
+}
+
+const { Label, Option } = HeadlesUiRadioGroup;
+
+export const RadioGroup: FC<RadioGroupProps> = ({
+  value,
+  onChange,
+  options,
+  className,
+}) => {
+  return (
+    <HeadlesUiRadioGroup
+      value={value}
+      onChange={onChange}
+      className={className}
+    >
+      {options.map((option) => (
+        <Option key={option.value} value={option.value}>
+          {({ checked }) => (
+            <Label className={checked ? 'text-blue-200' : ''}>
+              {option.label}
+            </Label>
+          )}
+        </Option>
+      ))}
+    </HeadlesUiRadioGroup>
+  );
+};
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ProductCard } from '../../components';
+import { ProductCard, RadioGroup } from '../../components';
 
 type ProductsResponseProps = {
   id: number;
@@ -15,19 +15,36 @@ type ProductsResponseProps = {
 }[];
 
 export const Home: React.FC = () => {
+  const [category, setCategory] = useState('');
   const [products, setProducts] = useState<ProductsResponseProps>([]);
 
+  const categoryOptions = [
+    { value: '', label: 'All' },
+    { value: 'category/electronics', label: 'Eletronics' },
+    { value: 'category/jewelery', label: 'Jewelery' },
+    { value: `category/men's%20clothing`, label: 'Mens clothing' },
+    { value: `category/women's%20clothing`, label: 'Womens clothing' },
+  ];
+
+  console.log(category);
+
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch(`https://fakestoreapi.com/products/${category}`)
       .then((res) => res.json())
       .then((json) => setProducts(json));
-  }, []);
+  }, [category]);
 
   console.log(products);
 
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-medium">Products</h1>
+      <RadioGroup
+        value={category}
+        onChange={setCategory}
+        options={categoryOptions}
+        className="inline-flex justify-center gap-4 pl-8 pb-8"
+      />
       <div className="grid grid-cols-3 justify-items-center gap-4">
         {products?.map((product) => (
           <ProductCard
